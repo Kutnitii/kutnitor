939ab9ad945c58947fdcf59ab9138a60c56fc170
Quentix <quentin.hay-kergrohenn@epita.fr>
2024-10-15 18:06:23 +0530

    d

--- lib/src/article/article_view.dart
+++ lib/src/article/article_view.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:kutnitor/src/article/web_view_stack.dart';
+import 'package:kutnitor/src/settings/settings_view.dart';
 import 'package:kutnitor/src/utils/http_client.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
-import 'article_bar.dart';
 
 class ArticleView extends StatefulWidget {
   const ArticleView({super.key});
@@ -16,38 +16,118 @@ class ArticleView extends StatefulWidget {
 }
 
 class _ArticleView extends State<ArticleView> {
-  late final WebViewController controller;
+  final WebViewController controller = WebViewController();
 
   int _articleId = 0;
+  List<String> _tags = List.empty();
+  
+  int _prevArticleId = 0;
+  List<String> _prevTags = List.empty();
 
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController();
-  }
+  Future<String> getNewMedia() {
+    return HttpClient().get('/review').then((response) {
+      Map<String, dynamic> decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
+      if (response.statusCode == 200) {
+        setState(() {
+          _prevArticleId = _articleId;
+          _prevTags = _tags;
+          _articleId = int.parse(decodedResponse.keys.first);
+          var tags = decodedResponse[_articleId.toString()]['heuristic_tags'];
+          print(decodedResponse);
+          print(tags.runtimeType);
+          print(tags);
+        });
+        return "";
+      }
 
-  void sendAndChange(String grade) async {
-    HttpClient().post('/rating/$_articleId', {'review': grade,});
-    var response = await HttpClient().get('/review');
-    Map<String, dynamic> decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
-    setState(() {
-      _articleId = int.parse(decodedResponse.keys.first);
-      controller.loadRequest(
-        Uri.parse(decodedResponse[_articleId.toString()]['url']),
-      );
+      return decodedResponse['error'];
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    getNewMedia();
+  }
+
+  void sendAndChange(String grade) async {
+    HttpClient().post('/rating/$_articleId', {'review': grade,});
+    getNewMedia();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<String> tags = [
-      "Spain", "India", "Space", "Competitive", "Alliance", "Ananas",
-      "Mango", "Changria", "Ariane", "Navrati", "Geometry", "Test",
-    ];
     return Scaffold(
-      appBar: ArticleTopBar(tags: tags),
-      body: WebViewWidget(controller: controller),
-      floatingActionButton: const ArticleBottomButtons(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.logout_outlined),
+          onPressed: () {
+            HttpClient().removeToken();
+            Navigator.pop(context);
+          },
+        ),
+        title: SizedBox(
+          height: const Size.fromHeight(kToolbarHeight).height,
+          child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.all(4.0),
+            separatorBuilder: (BuildContext context, int index) => const Divider(
+              indent: 4.0,
+            ),
+            itemCount: _tags.length,
+            itemBuilder:(BuildContext context, int index) {
+              return ElevatedButton(
+                onPressed: () {},
+                child: Text(_tags[index]),
+              );
+            },
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.replay),
+            onPressed: () {
+              controller.reload();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.restorablePushNamed(context, SettingsView.routeName)
+          ),
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <FloatingActionButton>[
+          FloatingActionButton(
+            heroTag: 'UndoButton',
+            onPressed: () {},
+            tooltip: 'Undo',
+            child: const Icon(Icons.undo),
+          ),
+          FloatingActionButton(
+            heroTag: 'PositiveButton',
+            onPressed: () => sendAndChange('Good'),
+            tooltip: 'Positive media',
+            child: const Icon(Icons.thumb_up),
+          ),
+          FloatingActionButton(
+            heroTag: 'NeutralButton',
+            onPressed: () => sendAndChange('Neutral'),
+            tooltip: 'Neutral media',
+            child: const Icon(Icons.sentiment_neutral),
+          ),
+          FloatingActionButton(
+            heroTag: 'NegativeButton',
+            onPressed: () => sendAndChange('Bad'),
+            tooltip: 'Negative media',
+            child: const Icon(Icons.thumb_down),
+          ),
+        ] 
+      ),
     );
   }
 }
